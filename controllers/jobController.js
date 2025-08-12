126c1412153935fe20582c060d9d9ed259939fa2
Sheifu33 <suffianakhtar@gmail.com>
2025-08-12 18:14:59 +0500

    Add select method to remove specified fields in jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,7 +5,7 @@ exports.getJobs = async (req, res, next) => {
   const companyID = req.query.companyID;
   try {
     const companyDoc = await Company.findOne({ ID: companyID });
-    const jobsArr = await Job.find({ company: companyDoc._id });
+    const jobsArr = await Job.find({ company: companyDoc._id }).select('-_id -__v -company');
     console.log(jobsArr);
     res.status(200).json({ message: 'Jobs List', jobs: jobsArr });
   } catch (err) {
