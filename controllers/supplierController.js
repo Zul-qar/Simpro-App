9bb708ae0edccc584a06fba64e186d1334b335ba
Sheifu33 <suffianakhtar@gmail.com>
2025-08-13 20:13:01 +0500

    Add vendor receipts functionality

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -1,15 +1,29 @@
 import Company from '../models/company.js';
 import VendorOrder from '../models/vendorOrder.js';
+import VendorReceipt from '../models/vendorReceipt.js';
 
 const getVendorOrders = async (req, res, next) => {
   const companyID = req.query.companyID;
   try {
     const companyDoc = await Company.findOne({ ID: companyID });
-    const vendorOrdersArr = await VendorOrder.find({ company: companyDoc._id });
+    const vendorOrdersArr = await VendorOrder.find({ company: companyDoc._id }).select('-__v -company -_id');
     res.status(200).json({ message: 'Vendor Orders List', vendorOrders: vendorOrdersArr });
   } catch (err) {
     next(err);
   }
 };
 
-export { getVendorOrders };
+const getVendorReceipts = async (req, res, next) => {
+  const companyID = req.query.companyID;
+  const vendorOrderID = req.query.vendorOrderID;
+  try {
+    const companyDoc = await Company.findOne({ ID: companyID });
+    const vendorOrderDoc = await VendorOrder.findOne({ ID: vendorOrderID });
+    const vendorReceiptsArr = await VendorReceipt.find({ company: companyDoc._id, vendorOrder: vendorOrderDoc._id });
+    res.status(200).json({ message: 'Vendor Receipts List', vendorReceipts: vendorReceiptsArr });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { getVendorOrders, getVendorReceipts };
--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -5,5 +5,6 @@ import * as supplierController from '../controllers/supplierController.js';
 const router = express.Router();
 
 router.get('/supplierorders', supplierController.getVendorOrders);
+router.get('/supplierreceipts', supplierController.getVendorReceipts);
 
 export default router;
